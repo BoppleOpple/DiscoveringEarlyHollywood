11c7e02bf0787247cc660be4d4eb7f1dafb0f7b3
BoppleOpple <boppleopple@gmail.com>
2025-12-03 15:05:51 -0500

    forgot to push changes

--- dbSetup/uploadData.py
+++ dbSetup/uploadData.py
@@ -18,8 +18,6 @@ parser.add_argument("-d", "--document-directory", default="./data/film_copyright
 parser.add_argument("-t", "--transcript-directory", default="./data/Hollywood_Copyright_Materials_Base_Transcriptions")
 parser.add_argument("-m", "--metadata-directory", required=False, default="./data/cleaned_copyright_with_metadata")
 
-parser.add_argument("-u", "--user", required=False, default="system")
-
 def createTables(cursor: psycopg2.extras._cursor):
 	print("Adding relations to database")
 	with open("tableDefinitions.sql", "r") as f:
@@ -27,21 +25,19 @@ def createTables(cursor: psycopg2.extras._cursor):
 
 def loadData(args: argparse.Namespace, cursor: psycopg2.extras._cursor):
 	print("parsing movies")
-	ids: list[str] = os.listdir(args.metadata_directory)
+	ids: list[str] = [fname[:-24] for fname in os.listdir(args.metadata_directory)]
 	movieData: list = []
 	transcriptData: list = []
 
 	for document_id in tqdm(ids):
-		currentMovieEntry = (
+		currentMovieEntry = [
 			document_id,
 			None,
 			None,
-			f"{args.document_directory}/{document_id}.pdf",
-			None,
 			datetime.datetime.now()
-		)
+		]
 
-		with open(f"{args.metadata_directory}/{document_id}") as metadataJson:
+		with open(f"{args.metadata_directory}/{document_id}with_added_metadata.json") as metadataJson:
 			metadata = json.load(metadataJson)
 
 			currentMovieEntry[1] = metadata["date"]
@@ -59,19 +55,42 @@ def loadData(args: argparse.Namespace, cursor: psycopg2.extras._cursor):
 	print("Adding documents to database")
 	psycopg2.extras.execute_batch(
 		cursor,
-		"INSERT INTO movies ( \
-			id \
-			copyright_year \
-			studio \
-			image_path \
+		"INSERT INTO documents ( \
+			id, \
+			copyright_year, \
+			studio, \
 			uploaded_time \
-		) VALUES (%s, %s, %s, %s, %s) \
+		) VALUES (%s, %s, %s, %s) \
 		ON CONFLICT DO NOTHING;",
-		movieData,
+		tqdm(movieData),
+		page_size=500
+	)
+
+	print("Adding transcripts to database")
+	psycopg2.extras.execute_batch(
+		cursor,
+		"INSERT INTO transcripts ( \
+			document_id, \
+			page_number, \
+			content \
+		) VALUES (%s, %s, %s) \
+		ON CONFLICT DO NOTHING;",
+		tqdm(transcriptData),
 		page_size=500
 	)
 
 
+def main(argv = None):
+	args = parser.parse_args(argv)
+	load_dotenv()
+
+	dbConnection: psycopg2.extensions.connection = psycopg2.connect(
+		host=os.environ["SQL_HOST"],
+		port=os.environ["SQL_PORT"],
+		dbname=os.environ["SQL_DBNAME"],
+		user=os.environ["SQL_USER"],
+		password=os.environ["SQL_PASSWORD"],
+	)
 
 	with dbConnection.cursor() as cursor:
 		try:
